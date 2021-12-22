351c738266aee19691095145943911573d8d9f05
helloladybear <96545478+helloladybear@users.noreply.github.com>
2021-12-22 10:39:05 -0800

    Add files via upload

---
+++ timestamp_test.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta, tzinfo
+# result = datetime.strptime('2017-01-12T14:12:06.000-0500','%Y-%m-%dT%H:%M:%S.%f%z')
+# result = datetime.strptime('2017-01-12T14:12:06.000-0500','%Y-%m-%dT%H:%M:%S')
+# print(result)
+
+# dt = datetime(2011, 11, 4, 0, 5, 23, 283000, tzinfo=datetime.timezone.utc)
+# print(dt)
